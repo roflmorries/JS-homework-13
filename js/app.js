a20554b17eb0fd0a077d1885743e88884ef38307
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-02-07 00:23:40 +0200

    add validation

--- js/app.js
+++ js/app.js
@@ -4,16 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   showCategories(); 
 });
 
-// let selectedCategory = null;
-
 document.querySelector(".categories").addEventListener("click", (event) => {
   if (
     event.target.tagName === "DIV" &&
     event.target.classList.contains("category")
   ) {
     const categoryId = event.target.getAttribute("data-id");
-
-    // selectedCategory = categoryId;
 
     document.querySelector(".products").setAttribute('data-category-id', categoryId);
 
@@ -82,7 +78,6 @@ function showProductInfo(product) {
   slider(product.images);
 
   const buyBtn = document.createElement('button');
-  // buyBtn.setAttribute('type', 'button');
   buyBtn.type = 'button';
   buyBtn.textContent = 'Buy now!';
   buyBtn.classList.add('buy-btn');
@@ -100,8 +95,29 @@ function showOrderForm() {
 document.querySelector('#order-btn').addEventListener('click', () => {
   const client = {
     name: document.forms.order.name.value,
+    phone: document.forms.order.phone.value,
     city: cities[document.forms.order.city.value],
   }
+
+  const validationName = /^[A-ZА-Я][a-zа-я]{1,}$/;
+  const validationPhoneNumber = /^(\+380|380|0)\d{9}$/;
+  const orderForm = document.getElementById('order_form');
+
+  if (!validationName.test(client.name) || !validationPhoneNumber.test(client.phone)) {
+    const notification = document.getElementById('notification_form');
+
+    orderForm.classList.add('invalid');
+    notification.innerHTML = `
+    <p>Ошибка! Форма заполнена неверно!</p>
+    `;
+
+    setTimeout(() => notification.innerHTML = '', 2000);
+    setTimeout(() => orderForm.classList.remove('invalid'), 1000);
+
+    console.log(client)
+    return;
+  }
+
   console.log(client)
   showNotification()
 })
